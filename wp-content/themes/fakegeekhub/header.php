1cd98c1e78662dc3a5f63497bd3e0ed24f9f08d3
Oleksandr Petukhov <o.petukhov@webkate.com>
2014-11-11 09:57:43 +0200

    made header

--- wp-content/themes/fakegeekhub/header.php
+++ wp-content/themes/fakegeekhub/header.php
@@ -8,7 +8,7 @@
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
  */
-?><!DOCTYPE html>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--[if IE 7]>
 <html class="ie ie7" <?php language_attributes(); ?>>
 <![endif]-->
@@ -19,18 +19,119 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta name="description" content="GeekHub надає можливість отримати практичні знання та навички в сфері розробки програмного забезпечення">
+    <meta name="keywords" content="GeekHub, ГікХаб, Черкаси, Cherkassy">
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<!--title: "GeekHub - отримай практичні знання в сфері IT"-->
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/flipclock.css">
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
+
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.6.4.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/libs/prefixfree.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/flipclock.min.js"></script>
+    <script type="text/javascript" src="http://userapi.com/js/api/openapi.js?34"></script>
+
+    <script type="text/javascript">
+        $(function(){
+
+            var t1 = new Date("September 17, 2014 00:00:00");
+            var t2 = new Date();
+            var seconds = (t1.getTime() - t2.getTime()) / 1000;
+
+            var Seconds_Between_Dates = Math.abs(seconds);
+
+            var clock = $('.countdown').FlipClock(Seconds_Between_Dates,{clockFace:'DailyCounter',countdown:true,	showSeconds: false});
+
+
+            function isEmailValid(email){
+                var pass = /^[a-z0-9._%+-]+@[a-z0-9._-]+\.[a-z]{2,6}$/i;
+                if(!pass.test(email)){
+                    return false;
+                }
+                return true;
+            }
+
+            $('.types li').click(function(){
+                var index = $(this).index();
+                window.location = '/details.html?'+index;
+                return false;
+            })
+
+            $('#header form').submit(function(){
+                var email = $(this).find('.email');
+                var loader = $(this).find('span');
+                var val = email.val();
+
+                if (!isEmailValid(val)) {
+                    email.addClass('error');
+                    email.focus();
+                    return false;
+                }
+                email.removeClass('error');
+                email.attr('disabled',true);
+                loader.fadeIn(300);
+
+                var data = { email: val };
+
+                $.post('/notify.php', data, function(){
+                    loader.fadeOut(300);
+                    email.attr('disabled',false);
+                    email.val('');
+                    alert('Готово');
+                });
+
+                return false;
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-27082120-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter17027506 = new Ya.Metrika({id:17027506, enableAll: true, webvisor:true});
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="//mc.yandex.ru/watch/17027506" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body  <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php if ( get_header_image() ) : ?>
 	<div id="site-header">
@@ -40,19 +141,48 @@
 	</div>
 	<?php endif; ?>
 
-	<header id="masthead" class="site-header" role="banner">
+	<div id="wrap">
+	<div id="header">
+		<h1><a href="/">GeekHub</a></h1>
+        <ul class="nav">
+            <li class="active"><a href="/">Головна</a></li>
+            <li><a href="/about.html">Про GeekHub</a></li>
+            <li><a href="/team.html">Команда</a></li>
+            <li><a href="/faq.html">FAQ</a></li>
+            <li><a href="/contacts.html">Зв'язок</a></li>
+        </ul>
+        <!--<div class="nav">
+            <?php wp_nav_menu(array('menu' => 'top-menu', 'menu_class' => 'top-menu')); ?>
+        </div>-->
+
+        <ul class="links">
+            <li class="fb"><a href="http://www.facebook.com/pages/GeekHub/158983477520070">facebook</a></li>
+            <li class="vk"><a href="http://vkontakte.ru/geekhub">Вконтакте</a></li>
+            <li class="tw"><a href="http://twitter.com/#!/geek_hub">twitter</a></li>
+            <li class="yb"><a href="http://www.youtube.com/user/GeekHubchannel">youtube</a></li>
+        </ul>
+        <span class="line"></span>
+
+        <h4 class="registration">Реєстрацію на 4й сезон закрито</h4>
+        <p class="note">*залиште нам ваш емейл і ми повідомимо вас про початок реєстрації</p>
+        <form action="#">
+            <fieldset>
+                <span></span>
+                <input type="text" class="email" placeholder="Ваш email" />
+                <input type="submit" value="Відіслати" />
+            </fieldset>
+
+        </form>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/splash.png" alt="splash" />
+	</div><!-- header -->
+
+	<!--<header id="masthead" class="site-header" role="banner">
 		<div class="header-main">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
 			<div class="search-toggle">
-				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
+				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'fakegeekhub' ); ?></a>
 			</div>
-
-			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
-				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-			</nav>
 		</div>
 
 		<div id="search-container" class="search-box-wrapper hide">
@@ -60,6 +190,6 @@
 				<?php get_search_form(); ?>
 			</div>
 		</div>
-	</header><!-- #masthead -->
+	</header>--><!-- #masthead -->
 
 	<div id="main" class="site-main">
